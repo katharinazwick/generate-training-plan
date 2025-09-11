9a4dcd97766bb2f457c44c98c8a732ff6d94902f
katharinazwick <katharinazwick.71@gmail.com>
2025-09-11 19:24:50 +0200

    database for trainingparts

---
+++ frontend/src/training.js
@@ -0,0 +1,81 @@
+export const training = [
+    {
+        name: "Verfolgungslauf 400m Zweier Partner mit Zeitabstand",
+        time: 10,
+        description:"sjdhgfkzsegfofhaksz etcrbkailnrucbdzsfkvbal-dövbkcjdfrgdhdö<-sdonfzuiwejuh",
+        part:"running",
+    },
+    {
+        name: "Verfolgungslauf 400m Gruppe mit Zeitabstand",
+        time: 10,
+        description:"",
+        part:"running",
+    },{
+        name: "Verfolgungslauf 400m Gruppe mit Zeitabstand",
+        time: 5,
+        description:"",
+        part:"running",
+    },
+    {
+        name: "Verfolgungslauf 400m Zweier Partner mit Streckenabstand",
+        time: 10,
+        description:"",
+        part:"running",
+    },
+    {
+        name: "Verfolgungslauf 400m Gruppe mit Streckenabstand",
+        time: 10,
+        description:"",
+        part:"running",
+    },
+    {
+        name: "1000m",
+        time: 10,
+        description:"",
+        part:"warmup",
+    },
+    {
+        name: "2000",
+        time: 15,
+        description:"",
+        part:"running",
+    },
+    {
+        name: "Lauf ABC",
+        time: 5,
+        description:"",
+        part:"warmup",
+    },
+    {
+        name: "Lauf ABC",
+        time: 5,
+        description:"",
+        part:"physicalStrength",
+    },
+    {
+        name: "Lauf ABC",
+        time: 5,
+        description:"",
+        part:"physicalStrength",
+    },{
+        name: "Lauf ABC",
+        time: 5,
+        description:"",
+        part:"coolDown",
+    },{
+        name: "Lauf ABC",
+        time: 5,
+        description:"",
+        part:"coolDown",
+    },{
+        name: "Lauf ABC",
+        time: 5,
+        description:"",
+        part:"coolDown",
+    }, {
+        name: "Lauf ABC",
+        time: 5,
+        description: "",
+        part: "physicalStrength",
+    },
+];
