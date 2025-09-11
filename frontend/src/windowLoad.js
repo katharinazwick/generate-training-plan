7e0e991610db1ac42f9d9cab03af8f47aac8f436
katharinazwick <katharinazwick.71@gmail.com>
2025-09-11 19:26:24 +0200

    reload the side

---
+++ frontend/src/windowLoad.js
@@ -0,0 +1,6 @@
+window.addEventListener("load", () => {
+    document.getElementById("warmupInput").value = "";
+    document.getElementById("runningInput").value = "";
+    document.getElementById("physicalStrengthInput").value = "";
+    document.getElementById("coolDownInput").value = "";
+});
