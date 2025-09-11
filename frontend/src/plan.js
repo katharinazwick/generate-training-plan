46689d2ae99d2e58e5f8b8d19e4062acb24b2e48
katharinazwick <katharinazwick.71@gmail.com>
2025-09-11 19:25:27 +0200

    get the complete plan

---
+++ frontend/src/plan.js
@@ -0,0 +1,10 @@
+import {getRandomTrainingForPart} from "./combination.js";
+
+export function getRandomTraining(timeForParts) {
+    return{
+        warmup: getRandomTrainingForPart('warmup', timeForParts),
+        running: getRandomTrainingForPart('running', timeForParts),
+        bodyWeight: getRandomTrainingForPart('physicalStrength', timeForParts),
+        coolDown: getRandomTrainingForPart('coolDown', timeForParts)
+    };
+}
