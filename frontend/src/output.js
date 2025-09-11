9e062e6cdab370d037b70f7e4da85a7580df1c29
katharinazwick <katharinazwick.71@gmail.com>
2025-09-11 20:19:40 +0200

    take the time to the header

--- frontend/src/output.js
+++ frontend/src/output.js
@@ -1,4 +1,4 @@
-import {getRandomTraining} from "./generatePlan.js";
+import {getRandomTraining} from "./generate/generatePlan.js";
 
 export function outputDatas(timeForParts) {
     const data = getRandomTraining(timeForParts);
@@ -11,8 +11,9 @@ export function outputDatas(timeForParts) {
 
         const title = document.createElement('div');
         title.className = 'part-title';
-        title.textContent = part.charAt(0).toUpperCase() + part.slice(1);
+        title.textContent = part.charAt(0).toUpperCase() + part.slice(1) + " " +  timeForParts[part];
         section.appendChild(title);
+        console.log(timeForParts)
 
         data[part].forEach(ex => {
             const row = document.createElement('div');
