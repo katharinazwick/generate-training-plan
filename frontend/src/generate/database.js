aaa5c6e2d52060b0ff83724c0745ab176f680d88
katharinazwick <katharinazwick.71@gmail.com>
2025-09-12 13:11:35 +0200

    include ids

--- frontend/src/generate/database.js
+++ frontend/src/generate/database.js
@@ -4,78 +4,92 @@ export const database = [
         time: 10,
         description:"sjdhgfkzsegfofhaksz etcrbkailnrucbdzsfkvbal-dövbkcjdfrgdhdö<-sdonfzuiwejuh",
         part:"running",
+        id: "1112"
     },
     {
         name: "Verfolgungslauf 400m Gruppe mit Zeitabstand",
         time: 10,
         description:"",
         part:"running",
+        id: "1113"
     },{
         name: "Verfolgungslauf 400m Gruppe mit Zeitabstand",
         time: 5,
         description:"",
         part:"running",
+        id: "1114"
     },
     {
         name: "Verfolgungslauf 400m Zweier Partner mit Streckenabstand",
         time: 10,
         description:"",
         part:"running",
+        id: "1115"
     },
     {
         name: "Verfolgungslauf 400m Gruppe mit Streckenabstand",
         time: 10,
         description:"",
         part:"running",
+        id: "1116"
     },
     {
         name: "1000m",
         time: 10,
         description:"",
         part:"warmup",
+        id: "1117"
     },
     {
         name: "2000",
         time: 15,
         description:"",
         part:"running",
+        id: "1118"
     },
     {
         name: "Lauf ABC",
         time: 5,
         description:"",
         part:"warmup",
+        id: "1119"
     },
     {
         name: "Lauf ABC",
         time: 5,
         description:"",
         part:"physicalStrength",
+        id: "1121"
     },
     {
         name: "Lauf ABC",
         time: 5,
         description:"",
         part:"physicalStrength",
+        id: "1122"
     },{
         name: "Lauf ABC",
         time: 5,
         description:"",
         part:"cooldown",
+        id: "1123"
     },{
         name: "Lauf ABC",
         time: 5,
         description:"",
         part:"cooldown",
+        id: "1124"
     },{
         name: "Lauf ABC",
         time: 5,
         description:"",
         part:"cooldown",
+        id: "1125"
     }, {
         name: "Lauf ABC",
         time: 5,
         description: "",
         part: "physicalStrength",
+        id: "1126"
     },
 ];
