fd67bea8338d18444f8acdb855a73ecf630e63cd
katharinazwick <katharinazwick.71@gmail.com>
2025-09-13 15:15:14 +0200

    delete fillschedule

--- frontend/src/link/checkUrl.js
+++ frontend/src/link/checkUrl.js
@@ -2,14 +2,14 @@ import {resolveExercises} from "./resolveExercises.js";
 import {outputDatas} from "../output/output.js";
 import {readUrlData} from "./readUrl.js";
 import {buttonIsClicked} from "../generate/clickButton.js";
-import {fillSchedule} from "../schedule/fillSchedule.js";
+import {scheduleClose} from "../schedule/scheduleClose.js";
 
 document.addEventListener("DOMContentLoaded", () => {
     const {data, meta} = readUrlData();
     const resolved = resolveExercises(data);
     if (Object.keys(data).length > 0) {
+        outputDatas(resolved, meta);
     }
-    outputDatas(resolved, meta);
-    fillSchedule(meta);
+    scheduleClose()
     buttonIsClicked();
 });
--- frontend/src/schedule/fillSchedule.js
+++
@@ -1,3 +0,0 @@
-export function fillSchedule() {
-    console.log("hfbshfbih")
-}
